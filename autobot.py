2dc9fe34041e9f47449671d8f1e0bf5d8beeed80
gowthamaraj <gowthamaraj360@gmail.com>
2021-04-12 23:46:01 +0530

    add IP feat mod

--- autobot.py
+++ autobot.py
@@ -1,8 +1,18 @@
 import yaml
 import shodan
 import argparse
-import json
 import pprint
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 # getting the secret key secretly ;p
 key = yaml.safe_load(open('api.yaml'))['key']
@@ -16,6 +26,7 @@ args = vars(parser.parse_args())
 try:
     # --ip
     if('ip' in args):
+        print(bcolors.HEADER,f"###  The output of ip:{args['ip']}   ###",bcolors.ENDC)
         result = api.host(args['ip'])
         pprint.pprint(result)
 except Exception as e:
