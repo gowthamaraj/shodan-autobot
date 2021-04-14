a91054dcb5d13740d32926e0f1e666df9158f9c3
gowthamaraj <gowthamaraj360@gmail.com>
2021-04-15 00:54:10 +0530

    add facets

--- autobot.py
+++ autobot.py
@@ -20,11 +20,17 @@ key = yaml.safe_load(open('api.yaml'))['key']
 api = shodan.Shodan(key)
 
 parser = argparse.ArgumentParser(description='Shodan Autobot Script')
+
+# --ip
 parser.add_argument('--ip', help='IP to scan', required=False)
+
+# --count
 parser.add_argument('--count-filters', help='Filter for the count. https://beta.shodan.io/search/examples' , required=False)
 parser.add_argument('--count-facets', help='Facets for the count. https://beta.shodan.io/search/facet' , required=False)
-parser.add_argument('--search-filters', help='Filter for the search. https://beta.shodan.io/search/examples' , required=False)
-parser.add_argument('--search-facets', help='Facets for the search. https://beta.shodan.io/search/facet' , required=False)
+
+# --facets
+parser.add_argument('--facets',help="List the facets", required=False, action='store_true')
+
 
 args = vars(parser.parse_args())
 
@@ -38,11 +44,23 @@ try:
         print("\n")
 
     # --count
-    if(args['count_filters'] and args['count_facets']):
+    if(args['count_filters'] is not None and args['count_facets'] is not None):
         print(bcolors.HEADER,f"###  The output of query : {args['count_filters']} | facets : {args['count_facets']} ###",bcolors.ENDC,"\n")
         result = api.count(query=args['count_filters'], facets=args['count_facets'])
         pprint.pprint(result)
         print("\n")
-        
+
+    # --facets
+    if(args['facets']):
+        print(bcolors.HEADER,f"###  List of Facets ###",bcolors.ENDC,"\n")
+        print(api.search_facets())
+        print("\n")
+
+    # --filters
+    if(args['filters']):
+        print(bcolors.HEADER,f"###  List of Filters ###",bcolors.ENDC,"\n")
+        print(api.search_filters())
+        print("\n")
+
 except Exception as e:
     print(e)
